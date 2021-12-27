2fcc83af53a0abedc9cf804f0aea811588953a70
dacaiguoguo <dacaiguoguo@163.com>
2021-12-27 14:48:08 +0800

    Update XcodeRecentManager.js

--- XcodeRecentManager.js
+++ XcodeRecentManager.js
@@ -14,7 +14,7 @@ const {
 } = objc;
 
 var tempPath = "/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.apple.dt.xcode.sfl2"
-
+// tempPath 是要相对于用户目录的。所以要获取用户目录进行拼接。/Users/username/tempPath
 var path = NSString.stringWithString(userInfo['homedir'] + tempPath);
 
 console.log(path)
@@ -32,6 +32,7 @@ var recentList = recentListInfo.objectForKey("items")
 for (var i = recentList.count() - 1; i >= 0; i--) {
     var firstObj = recentList.objectAtIndex(i)
     var bookmarkData = firstObj.objectForKey("Bookmark")
+    // 由于bookmarkData是系统用Objective-C的方式写入的。也只能用Objective-C的方式读取。如下：
     var recentItemurl = NSURL.URLByResolvingBookmarkData_options_relativeToURL_bookmarkDataIsStale_error(bookmarkData, 1 << 8, null, null, null)
     console.log(String(recentItemurl), typeof(String(recentItemurl)))
 }
