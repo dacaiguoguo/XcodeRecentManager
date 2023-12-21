4c273eddc26b6110282d1b746a4375858d2a2a96
dacaiguoguogmail <dacaiguoguo@163.com>
2023-12-21 16:24:27 +0800

    update

--- ReadApplicationRecentDocuments/ReadApplicationRecentDocuments/SFLListItem.m
+++ ReadApplicationRecentDocuments/ReadApplicationRecentDocuments/SFLListItem.m
@@ -6,54 +6,6 @@
 //
 
 #import "SFLListItem.h"
-// com.apple.finder.sfl 需要测试
-NSArray* readSflWithFile(NSString *filePath) {
-    NSFileManager *fileManager = [NSFileManager defaultManager];
-    BOOL isExist = [fileManager fileExistsAtPath:filePath];
-    if (!isExist) {
-        return nil;
-    }
-    NSURL *fileUrl = [NSURL fileURLWithPath:filePath];
-    NSError *err = nil;
-    NSData *data = [[NSData alloc] initWithContentsOfURL:fileUrl options:(NSDataReadingMappedIfSafe) error:&err];
-    if (err || data == nil) {
-        NSLog(@"%@",err.localizedDescription);
-        return nil;
-    }
-    NSDictionary *recentListInfo = [NSKeyedUnarchiver unarchiveTopLevelObjectWithData:data error:&err];
-    NSArray *recentList = recentListInfo[@"items"];
-
-    NSMutableArray *mutArray = [NSMutableArray array];
-    if ([recentList.firstObject isKindOfClass:NSDictionary.class]) {
-        [recentList enumerateObjectsUsingBlock:^(NSDictionary *bookmarkInfo, NSUInteger idx, BOOL * _Nonnull stop) {
-            NSData *bookmark = bookmarkInfo[@"Bookmark"];
-            NSError *resolveError = nil;
-            NSURL *resolvedUrl = [NSURL URLByResolvingBookmarkData:bookmark options:(NSURLBookmarkResolutionWithoutUI) relativeToURL:nil bookmarkDataIsStale:nil error:&resolveError];
-            if (resolvedUrl == nil) {
-                // NSLog(@"%@",@"null");
-                return;
-            }
-            // NSLog(@"%@",resolvedUrl.path);
-            [mutArray addObject:resolvedUrl.path];
-
-        }];
-    } else if ([recentList.firstObject isKindOfClass:SFLListItem.class]) {
-        [recentList enumerateObjectsUsingBlock:^(SFLListItem *bookmarkInfo, NSUInteger idx, BOOL * _Nonnull stop) {
-            NSURL *resolvedUrl = bookmarkInfo.URL;
-            if (resolvedUrl == nil) {
-                // NSLog(@"%@",@"null");
-                return;
-            }
-            // NSLog(@"%@",resolvedUrl.path);
-            [mutArray addObject:resolvedUrl.path];
-
-        }];
-    }
-
-
-    // NSLog(@"%@", mutArray);
-    return mutArray.copy;;
-}
 
 @implementation SFLListItem
 + (BOOL)supportsSecureCoding {
